422782f83cddf6f938c79cfe819db759b857581e
Nikhil Chetanbhai Chhasiya <153722480+NIKHIL-07-CYBER@users.noreply.github.com>
2026-01-31 16:24:44 +0530

    "fixing unnecessary folder push"

--- live/liveness-detection/liveness-detection/src/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Utility modules for liveness detection.
-"""
-
-from .face_detector import FaceDetector
-from .blink_detector import BlinkDetector
-from .anti_spoofing import AntiSpoofing
-
-__all__ = ["FaceDetector", "BlinkDetector", "AntiSpoofing"]
